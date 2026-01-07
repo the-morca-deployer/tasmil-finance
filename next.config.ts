b951d722938fd5ca1789d5739dbd76f258f0ff1e
nathanielfsn <phuongtn@mirailabs.co>
2026-01-08 01:29:13 +0700

    fix: frontend build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: false, 
-  /* config options here */
+  reactStrictMode: false,
 };
 
 export default nextConfig;
