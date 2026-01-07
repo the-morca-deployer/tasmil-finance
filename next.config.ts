9d100bd1eb9e20ca6bf07c70f845d78d75bc0c04
nathanielfsn <phuongtn@mirailabs.co>
2026-01-08 01:48:32 +0700

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  // Exclude kubb config from build
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  // Exclude files from compilation
-  experimental: {
-    serverComponentsExternalPackages: ['@kubb/core', '@kubb/cli', '@kubb/plugin-oas', '@kubb/plugin-ts', '@kubb/plugin-react-query'],
-  },
 };
 
 export default nextConfig;
